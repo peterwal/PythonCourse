d83387d653157a973057056cf2a8f1a519c2ef94
PWallner <wallner17@thewallners.net>
2022-11-01 19:17:54 -0500

    committing GUI

--- TkInter_PasswordManager/main.py
+++ TkInter_PasswordManager/main.py
@@ -1,5 +1,40 @@
+from tkinter import *
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager")
+window.config(padx=50, pady=50)
+canvas = Canvas(width=200, height=200)
+image = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=image)
+canvas.grid(row=0, column=1)
+
+website_label =  Label(text="Website")
+website_label.grid(row=1, column=0)
+email_label = Label(text="email/User")
+email_label.grid(row=2, column=0)
+password_label = Label(text="password")
+password_label.grid(row=3, column=0)
+
+website_entry = Entry(width=35)
+website_entry.grid(row=1, column=1, columnspan=2)
+email_entry = Entry(width=35)
+email_entry.grid(row=2, column=1, columnspan=2)
+password_entry = Entry(width=21)
+password_entry.grid(row=3, column=1)
+
+generate_passw_button = Button(text="Generate password")
+generate_passw_button.grid(row=3, column=2)
+add_button = Radiobutton(text="add", width=36)
+add_button.grid(row=4, column=1, columnspan=2)
+
+
+
+
+
+
+mainloop()
